f40b1d70611aeab721aa1d8c986ab2a2ecdffd4b
jpforcen <1686566+jpforcen@users.noreply.github.com>
2022-09-23 15:09:23 -0700

    fix: increasing number of tries

--- src/constants.ts
+++ src/constants.ts
@@ -50,4 +50,4 @@ export const MIGRATIONS_DIR = path.join(
 
 export const CONTENTFUL_ALIAS = "master";
 export const DELAY = 3000;
-export const MAX_NUMBER_OF_TRIES = 10;
+export const MAX_NUMBER_OF_TRIES = 20;
